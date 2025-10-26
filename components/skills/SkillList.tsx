710a6c99698ca1b2ef2af324f49a91e36d89bc8f
Delwin444 <justinkuhn@t-online.de>
2025-10-26 22:43:48 +0100

    Add skill sorting, first per level, second alphabetically

--- components/skills/SkillList.tsx
+++ components/skills/SkillList.tsx
@@ -4,7 +4,10 @@ import {SkillElement} from "del/components/skills/SkillElement";
 export type SkillListType = Record<string, number>;
 
 
-export const SkillList = ({skills}: {skills: SkillListType}): ReactElement => {
+export const SkillList = ({skills}: { skills: SkillListType }): ReactElement => {
+    skills = Object.fromEntries(Object.entries(skills).sort((a, b) => {
+        return b[1] - a[1] || a[0].localeCompare(b[0]);
+    }));
     return (
         <>
             <section className="grid grid-cols-2">
