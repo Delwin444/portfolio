219462fa2fbe3fd4b60a2b008e270e07d69d0d10
Delwin444 <justinkuhn@t-online.de>
2025-11-10 00:10:52 +0100

    Improve Magento detail component

--- components/skills/skillDetails/backendFrameworks/Magento.tsx
+++ components/skills/skillDetails/backendFrameworks/Magento.tsx
@@ -13,32 +13,29 @@ const Magento = () => {
             <p>
                 2019 habe ich die ich Professional Zertifizierung abgeschlossen:
             </p>
-            <a href="https://www.credly.com/badges/535d92df-7eef-4406-bad2-520fe5b6bf66" target="_blank" className="inline-block p-2 border hover:underline mb-4">
+            <a href="https://www.credly.com/badges/535d92df-7eef-4406-bad2-520fe5b6bf66" target="_blank" className="inline-block p-3 border hover:underline mb-4 rounded-2xl">
                 Magento 2 Professional
             </a>
             <p>
                 Sowie 2020 die Frontend Experten Zertifizierung:
             </p>
-            <a href="https://www.credly.com/badges/00f92721-a59f-449d-b528-fecfdc7e19a7" target="_blank" className="inline-block p-2 border hover:underline mb-4">
+            <a href="https://www.credly.com/badges/00f92721-a59f-449d-b528-fecfdc7e19a7" target="_blank" className="inline-block p-3 border hover:underline mb-4 rounded-2xl">
                 Magento 2 Frontend Expert
             </a>
             <p>
                 Diese habe ich 2025 nochmal abgeschlossen:
             </p>
-            <div className="flex gap-4">
-                <a href="https://certification.adobe.com/credential/verify/b2f1ab1e-a620-11f0-8ca0-42010a400fd3"
-                   target="_blank"
-                   className="flex-1">
-                    <Image src="/img/skills/frontend-certification.png" alt="Magento 2 Frontend Expert Zertifizierung Badge"
-                           placeholder="blur" blurDataURL="/img/skills/frontend-certification-blur.jpg"
-                           width={480} height={479}/>
-                </a>
-                <p className="flex-1">
-                    <a href="https://certification.adobe.com/credential/verify/b2f1ab1e-a620-11f0-8ca0-42010a400fd3" target="_blank" className="inline-block p-2 border hover:underline mb-4">
-                        Magento 2 Frontend Expert
-                    </a>
-                </p>
-            </div>
+            <a href="https://certification.adobe.com/credential/verify/b2f1ab1e-a620-11f0-8ca0-42010a400fd3"
+               target="_blank"
+               className="mb-4 block">
+                <Image src="/img/skills/frontend-certification.png" alt="Magento 2 Frontend Expert Zertifizierung Badge"
+                       placeholder="blur" blurDataURL="/img/skills/frontend-certification-blur.jpg"
+                       className="w-1/2 rounded-2xl"
+                       width={480} height={479}/>
+            </a>
+            <a href="https://certification.adobe.com/credential/verify/b2f1ab1e-a620-11f0-8ca0-42010a400fd3" target="_blank" className="inline-block p-3 border hover:underline mb-4 rounded-2xl">
+                Magento 2 Frontend Expert
+            </a>
         </>
     );
 };
