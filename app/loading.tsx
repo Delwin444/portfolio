845dfa0e92366014abf44c57f6c81e24d464cb4a
Delwin444 <justinkuhn@t-online.de>
2025-11-10 19:06:50 +0100

    remove loading background

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Loading() {
     return (
-        <div className="fixed top-0 left-0 w-full h-full z-100 flex items-center justify-center bg-black/20">
+        <div className="fixed top-0 left-0 w-full h-full z-100 flex items-center justify-center">
             <CircularProgress className="self-center"/>
         </div>
     )
